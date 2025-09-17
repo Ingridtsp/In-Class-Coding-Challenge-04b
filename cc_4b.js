40dc505bde211f9f0ff7841454cf53d3706cc36b
Ingridtsp <ingridnicole@usf.edu>
2025-09-17 19:51:45 -0400

    completed step 2 array of products

---
+++ cc_4b.js
@@ -0,0 +1,8 @@
+//Step 2: Define array of products 
+let products =[
+    { sku:"E100", name:"Laptop", category:"electronics", price:1095.00, inventory:12 },
+    { sku:"E200", name:"Mouse", category:"electronics", price:23.99, inventory:18 },
+    { sku:"E300", name:"Shoes", category:"apparel", price:125.00, inventory:27 },
+    { sku:"E400", name:"Sweater", category:"apparel", price:37.95, inventory:32 },
+    { sku:"E500", name:"Ice Cream", category:"groceries", price:7.95, inventory:12 },
+]
